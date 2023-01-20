035cf268b9c68ddaed774c32ee8046edcf027a42
Damian <AdrianAndradeChristian@outlook.com>
2023-01-20 09:05:07 -0500

    Implementing Paging

---
+++ AccountOwnerServer/Entities/Models/QueryStringParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace Entities.Models
+{
+    public class QueryStringParameters
+    {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1; // Data's page that I want to see
+
+        private int _pageSize = 10; // Numbers of rows I want to see
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
