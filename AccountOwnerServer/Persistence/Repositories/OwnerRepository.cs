6078a02f13a4aab5634d95478af85ae8226d0bcb
Christian <adrianAndradeChristian@gmail.com>
2023-01-20 12:18:43 -0500

    Done

--- AccountOwnerServer/Persistence/Repositories/OwnerRepository.cs
+++ AccountOwnerServer/Persistence/Repositories/OwnerRepository.cs
@@ -79,8 +79,11 @@ namespace Persistence.Repositories
             }
         }
 
+        // We need System.Linq.Dynamic.Core nuGet package
+        // Note: Name ascending, DateOfBirth descending is the format expected in the request for ordering
         private void ApplySort(ref IQueryable<Owner> owners, string orderByQueryString)
         {
+            // some mandatory checks for owners and queryString
             if (!owners.Any())
                 return;
 
@@ -90,31 +93,40 @@ namespace Persistence.Repositories
                 return;
             }
 
+            // splitting our query string to get the individual fields
             string[] orderParams = orderByQueryString.Trim().Split(',');
-
+            
+            // using reflection to prepare the list of PropertyInfo objects that represent the properties of our Owner class
+            // We need them to be able to check if the field received through the query string really exists in the Owner class
             PropertyInfo[] propertyInfos = typeof(Owner).GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             var orderQueryBuilder = new StringBuilder();
-
+            
+            // through all the parameters 
             foreach (var param in orderParams)
             {
                 if (string.IsNullOrWhiteSpace(param))
                     continue;
 
                 var propertyFromQueryName = param.Split(" ")[0];
-
+                
+                // check parameter's existence
                 var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
+                
+                // If we don’t find such a property we skip the step in the foreach loop and go to the next parameter in the list
                 if (objectProperty == null)
                     continue;
 
                 var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
-
+                
+                // use the StringBuilder to build our query with each loop
                 orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
             }
-
+            
+            // removing excess commas 
             var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-
+            
+            //one last check to see if our query indeed has something in it
             if (string.IsNullOrWhiteSpace(orderQuery))
             {
                 owners = owners.OrderBy(x => x.Name);
