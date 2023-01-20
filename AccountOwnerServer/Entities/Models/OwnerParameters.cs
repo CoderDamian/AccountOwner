87644fa358eb938245ee7ce7bb3a68b986be0742
Damian <AdrianAndradeChristian@outlook.com>
2023-01-20 12:09:50 -0500

    Implementing sorting

--- AccountOwnerServer/Entities/Models/OwnerParameters.cs
+++ AccountOwnerServer/Entities/Models/OwnerParameters.cs
@@ -6,5 +6,10 @@
         public uint MaxYearOfBirth { get; set; } = (uint)DateTime.Now.Year;
         public bool ValidYearRange => MaxYearOfBirth > MinYearOfBirth;
         public string Name { get; set; } = string.Empty;
+
+        public OwnerParameters()
+        {
+            OrderBy = "name";
+        }
     }
 }
--- AccountOwnerServer/Persistence/Repositories/OwnerRepository.cs
+++ AccountOwnerServer/Persistence/Repositories/OwnerRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Entities.Models;
 using Persistence.Contracts;
 using Persistence.Seedwork;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
 
 namespace Persistence.Repositories
 {
@@ -60,7 +63,7 @@ namespace Persistence.Repositories
             IQueryable<Owner> owners = FindAll()
                 .Where(o => o.DateOfBirth.Year >= ownerParameters.MinYearOfBirth && o.DateOfBirth.Year <= ownerParameters.MaxYearOfBirth)
                 .OrderBy(o => o.Name);
-            
+
             SearchByName(ref owners, ownerParameters.Name);
 
             return PagedList<Owner>.ToPagedList(owners,
@@ -74,6 +77,51 @@ namespace Persistence.Repositories
             {
                 owners.Where(o => o.Name.ToLower().Contains(name.Trim().ToLower()));
             }
+        }
+
+        private void ApplySort(ref IQueryable<Owner> owners, string orderByQueryString)
+        {
+            if (!owners.Any())
+                return;
+
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                owners = owners.OrderBy(o => o.Name);
+                return;
+            }
+
+            string[] orderParams = orderByQueryString.Trim().Split(',');
+
+            PropertyInfo[] propertyInfos = typeof(Owner).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+            {
+                owners = owners.OrderBy(x => x.Name);
+                return;
+            }
+
+            owners = owners.OrderBy(orderQuery);
         }
     }
 }
